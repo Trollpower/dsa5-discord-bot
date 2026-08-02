da1afb0a1aa4367b21655235994ef7f1ce3d2973
Trollpower <github@trollpower.de>
2026-08-02 18:20:51 +0200

    feat: enhance calendar command; add calculation subcommand and embed for date range

--- commands/kalender.js
+++ commands/kalender.js
@@ -1,5 +1,8 @@
 import { SlashCommandBuilder } from 'discord.js';
 import path from 'path';
+import kalender from '../data/kalender.json' with { type: 'json' };
+
+const kalenderChoices = kalender.map(month => ({ name: month.name, value: month.name }));
 
 export default {
 	data: new SlashCommandBuilder()
@@ -7,5 +10,28 @@ export default {
 		.setDescription('Kalender-Befehl')
 		.addSubcommand(subcommand => subcommand
 			.setName('show')
-			.setDescription('Zeigt den DSA-Kalender an')),
+			.setDescription('Zeigt den DSA-Kalender an'))
+		.addSubcommand(subcommand => subcommand
+			.setName('calc')
+			.setDescription('Berechnet die Tage zwischen zwei Kalenderdaten')
+			.addIntegerOption(option => option
+				.setName('starttag')
+				.setDescription('Starttag')
+				.setMinValue(1)
+				.setRequired(true))
+			.addStringOption(option => option
+				.setName('startmonat')
+				.setDescription('Startmonat')
+				.addChoices(...kalenderChoices)
+				.setRequired(true))
+			.addIntegerOption(option => option
+				.setName('endtag')
+				.setDescription('Endtag')
+				.setMinValue(1)
+				.setRequired(true))
+			.addStringOption(option => option
+				.setName('endmonat')
+				.setDescription('Endmonat')
+				.addChoices(...kalenderChoices)
+				.setRequired(true))),
 };
